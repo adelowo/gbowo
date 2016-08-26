e118534d34c1cc3165d76bcb70d1dfb88a41a974
Lanre Adelowo <me@adelowolanre.com>
2016-08-26 10:11:39 +0100

    Removing check for function definition of namespaced `env` function since some frameworks e.g laravel already define it hence leading to ours not be called. Function definition is still namespaced though. Renamed transaction reference function

--- src/Gbowo/functions.php
+++ src/Gbowo/functions.php
@@ -2,28 +2,25 @@
 
 namespace Gbowo;
 
-if (!function_exists('env')) {
-
-    /**
-     * Load a value from `$_ENV`.
-     * Bad thing is that it (`function_exists`) checks the global functions [not namespaced]
-     * @param string $value
-     * @return string|false
-     */
-    function env(string $value)
-    {
-        return getenv($value);
-    }
+/**
+* Load a value from `$_ENV`.
+* Cannot use `function_exists` check since it (`function_exists`) checks the global functions [not namespaced]
+* @param string $value
+* @return string|false
+ */
+function env(string $value)
+{
+    return getenv($value);
 }
 
-if (!function_exists('genTransRef')) {
+if (!function_exists('generate_trans_ref')) {
 
     /**
      * Generate a cryptographically secure random string
      * @param int $length Defaults to 10
      * @return string
      */
-    function genTransRef(int $length = 10)
+    function generate_trans_ref(int $length = 10)
     {
         return bin2hex(random_bytes($length));
     }
