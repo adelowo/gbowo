8c9ccc69ab88cb6c926aecda16c5c720a19372f0
Lanre Adelowo <me@adelowolanre.com>
2016-10-28 14:11:12 +0100

    Plugin to fecth all plans from amplifypay

---
+++ src/Gbowo/Adapter/Amplifypay/Plugin/FetchAllPlans.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace Gbowo\Adapter\Amplifypay\Plugin;
+
+use function GuzzleHttp\json_decode;
+use Gbowo\Plugin\AbstractFetchAllPlans;
+use Psr\Http\Message\ResponseInterface;
+use Gbowo\Exception\InvalidHttpResponseException;
+
+/**
+ * @author Lanre Adelowo <me@adelowolanre.com>
+ * Class FetchAllPlans
+ * @package Gbowo\Adapter\Amplifypay\Plugin
+ */
+class FetchAllPlans extends AbstractFetchAllPlans
+{
+
+    const ALL_PLANS_RELATIVE_LINK = "/plan?merchantId=:m&apiKey=:key";
+
+    /**
+     * @var string
+     */
+    protected $baseUrl;
+    /**
+     * @var array
+     */
+    protected $apiKeys;
+
+    public function __construct(string $baseUrl, array $apiKeys)
+    {
+        $this->apiKeys = $apiKeys;
+        $this->baseUrl = $baseUrl;
+    }
+
+    /**
+     * @return mixed
+     * @throws \Gbowo\Exception\InvalidHttpResponseException if the response status code is not 200
+     */
+    public function handle()
+    {
+
+        $link = $this->baseUrl. str_replace(":m",$this->apiKeys['merchantId'],self::ALL_PLANS_RELATIVE_LINK);
+
+        $link = str_replace(":key", $this->apiKeys['apiKey'], $link);
+
+        /**
+         * @var ResponseInterface $response
+         */
+        $response = $this->adapter->getHttpClient()
+            ->get($link);
+
+        if (200 !== $response->getStatusCode()) {
+            throw new InvalidHttpResponseException(
+                "Expected 200. Got{$response->getStatusCode()} instead"
+            );
+        }
+
+        return json_decode($response->getBody(), true);
+    }
+}
---
+++ tests/Adapter/Amplifypay/Plugin/FetchAllPlansTest.php
@@ -0,0 +1,122 @@
+<?php
+
+
+namespace Adapter\Amplifypay\Plugin;
+
+
+use Gbowo\Adapter\Amplifypay\AmplifypayAdapter;
+use Gbowo\Adapter\Amplifypay\Plugin\FetchAllPlans;
+use Gbowo\Tests\Mockable;
+use function Gbowo\env;
+
+class FetchAllPlansTest extends \PHPUnit_Framework_TestCase
+{
+
+    use Mockable;
+
+    /**
+     * @var array
+     */
+    protected $apiKeys;
+
+    public function setUp()
+    {
+        $this->apiKeys = [
+            "merchantId" => env("AMPLIFYPAY_MERCHANT_ID"),
+            "apiKey" => env("AMPLIFYPAY_API_KEY")
+        ];
+    }
+
+    /**
+     * @param $response
+     * @dataProvider getAllPlans
+     */
+    public function testFetchAllPlansPluginIsCalled($response)
+    {
+        $mockedResponse = $this->getMockedResponseInterface();
+
+        $httpClient = $this->getMockedGuzzle();
+
+        $httpClient->shouldReceive('get')
+            ->once()
+            ->andReturn($mockedResponse);
+
+        $mockedResponse->shouldReceive('getStatusCode')
+            ->once()
+            ->andReturn(200);
+
+        $mockedResponse->shouldReceive('getBody')
+            ->once()
+            ->andReturn($response);
+
+
+        $amplifyPay = (new AmplifypayAdapter($httpClient))->addPlugin(
+            new FetchAllPlans(AmplifypayAdapter::BASE_URL, $this->apiKeys)
+        );
+
+        $receivedResponse = $amplifyPay->fetchAllPlans();
+
+        $this->assertEquals($response, $receivedResponse);
+    }
+
+
+    /**
+     * @expectedException \Gbowo\Exception\InvalidHttpResponseException
+     * @expectedExceptionMessage Expected 200
+     */
+    public function testAnInvalidHttpResponseIsReceived()
+    {
+        $mockedResponse = $this->getMockedResponseInterface();
+
+        $httpClient = $this->getMockedGuzzle();
+
+
+        $httpClient->shouldReceive('get')
+            ->once()
+            ->andReturn($mockedResponse);
+
+        $mockedResponse->shouldReceive('getStatusCode')
+            ->twice()
+            ->andReturn(201);
+
+        $mockedResponse->shouldReceive('getBody')
+            ->never()
+            ->andReturnNull();
+
+        $amplifyPay = (new AmplifypayAdapter($httpClient))->addPlugin(
+            new FetchAllPlans(AmplifypayAdapter::BASE_URL, $this->apiKeys)
+        );
+
+        $receivedResponse = $amplifyPay->fetchAllPlans();
+    }
+
+
+    public function getAllPlans()
+    {
+        return [
+            [
+                "PlanId" => 1,
+                "PlanName" => "Platinum Dudes",
+                "Frequency" => "Custom",
+                "Params" => [
+                    "CustomNum" => 7,
+                    "CustomDay" => "Day"
+                ],
+                "DateCreated" => (new \DateTime())->setTimestamp(strtotime("20 years ago"))->format("Y-m-d")
+            ],
+            [
+                "PlanId" => 234,
+                "PlanName" => "Silver Guyz",
+                "Frequency" => "Monthly",
+                "DateCreated" => (new \DateTime())->setTimestamp(strtotime("13 years ago"))->format("Y-m-d")
+            ],
+            [
+                "PlanId" => 432,
+                "PlanName" => "Regular Homies",
+                "Frequency" => "Yearly",
+                "DateCreated" => (new \DateTime())->setTimestamp(strtotime("last year"))->format("Y-m-d")
+            ]
+        ];
+
+    }
+}
