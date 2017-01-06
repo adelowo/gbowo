9787e1f9782ff5f42f8727de2b51020497cc8e1f
Lanre Adelowo <me@lanreadelowo.com>
2017-01-06 03:37:02 +0100

    Removing redundant check for handleable plugins

--- src/Gbowo/Contract/Plugin/PluginInterface.php
+++ src/Gbowo/Contract/Plugin/PluginInterface.php
@@ -4,16 +4,10 @@ namespace Gbowo\Contract\Plugin;
 
 use Gbowo\Contract\Adapter\AdapterInterface;
 
-/**
- * @author Lanre Adelowo <me@adelowolanre.com>
- * Interface Plugin
- * @package Gbowo\Contract\Plugin
- */
 interface PluginInterface
 {
 
     public function getPluginAccessor() : string;
-
 
     public function setAdapter(AdapterInterface $adapter);
 
