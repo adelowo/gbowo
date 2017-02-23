508f89df5bb6974263f7c952da2deed5367637a7
Lanre Adelowo <me@lanreadelowo.com>
2017-02-23 09:24:00 +0100

    Code style fix/consistency.. Make sure all functions are camelCased

--- src/Gbowo/functions.php
+++ src/Gbowo/functions.php
@@ -3,29 +3,25 @@
 namespace Gbowo;
 
 if (!function_exists("Gbowo\\env")) {
-
     /**
      * * Load a value from `$_ENV`.
      * @param string $value
      * @return mixed
      */
-
     function env(string $value)
     {
         return $_ENV[$value];
     }
 }
 
-if (!function_exists("Gbowo\\generate_trans_ref")) {
-
+if (!function_exists("Gbowo\\generateTransRef")) {
     /**
      * Generate a cryptographically secure random string
      * @param int $length Defaults to 10
      * @return string
      */
-    function generate_trans_ref(int $length = 10)
+    function generateTransRef(int $length = 10)
     {
         return bin2hex(random_bytes($length));
     }
-
 }
